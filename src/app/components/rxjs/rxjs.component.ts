dffc1a66b5f2e717e55253de22a25b614ff313fd
Ketevan Nanuashvili <ketinanuashvili@gmail.com>
2024-08-07 01:35:45 +0400

    add of operator

--- src/app/components/rxjs/rxjs.component.ts
+++ src/app/components/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -10,8 +10,41 @@ export class RxjsComponent implements OnInit {
 
   agents: Observable<string>;
   agentsName: string;
+  studentsList = ['mark', 'ram', 'sita', 'lisa'];
+  studentObj={
+    id:10,
+    name: 'Keti'
+  }
+
+
+  students: Observable <string[]> = of(this.studentsList);
+
+  studentNames: Observable <string> = of ('keti');
+
+  student$: Observable<any> = of(this.studentObj);
+
+  // students: Observable <string[]> = of(['mark', 'ram', 'sita', 'lisa'])
 
   ngOnInit(): void{
+    this.students.subscribe (data=>{
+      console.log(data);
+      
+    });
+
+    this.studentNames.subscribe (data=>{
+      console.log(data);
+      
+    });
+
+    this.student$.subscribe(data=>{
+      console.log(data.id);
+      
+    })
+
+
+
+
+
     this.agents= new Observable(
       function(observer){
         try{
